27e2327a0dc74f90c30034bdaf5e30d479a157c0
Md Shagor <itzshagor9@gmail.com>
2026-04-05 09:54:07 +0600

    add: adding lab code 06

---
+++ introduction-to-programming/LAB/lab_code_06.c
@@ -0,0 +1,30 @@
+// Name: Md Shagor
+// Student Id: 0326725111
+// Studnet Roll: 72B/5111
+// Lab Report: 06
+// Experiment Name: Factorial Calculation
+
+#include <stdio.h>
+
+int factorial(int n) {
+    int ans = 1, i;
+    for (i = 2; i <= n; i++)
+    {
+        ans *= i;
+    }
+    return ans;
+}
+
+int main() {
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Error! Factorial of a negative number doesn't exist.\n");
+    } else {
+        printf("Factorial of %d = %d\n", num, factorial(num));
+    }
+
+    return 0;
+}
