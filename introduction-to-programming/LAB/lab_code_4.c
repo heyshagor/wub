c3a3bd64e9532f334189c0eeb96c6217c7791b53
Md Shagor <itzshagor9@gmail.com>
2026-04-05 09:35:36 +0600

    add: lab code 4: reverse of a number

---
+++ introduction-to-programming/LAB/lab_code_4.c
@@ -0,0 +1,25 @@
+// Name: Md Shagor
+// Student Id: 0326725111
+// Studnet Roll: 72B/5111
+// Lab Report: 03
+// Experiment Name: reverse of a number
+
+#include <stdio.h>
+
+int main() {
+    int num, reversed = 0, remainder;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+
+    printf("Reversed number: %d\n", reversed);
+
+    return 0;
+
+}
